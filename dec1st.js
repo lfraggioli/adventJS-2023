12105300f5425a7ee6a681b763c91a355de726c7
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2023-12-03 23:50:08 -0300

    first commit

---
+++ dec1st.js
@@ -0,0 +1,17 @@
+/* //* En la fábrica de juguetes del Polo Norte, cada juguete tiene un número de identificación único.
+ //* Sin embargo, debido a un error en la máquina de juguetes, algunos números se han asignado a más de un juguete.
+ //* ¡Encuentra el primer número de identificación que se ha repetido, donde la segunda ocurrencia tenga el índice más pequeño!
+ //* En otras palabras, si hay más de un número repetido, debes devolver el número cuya segunda ocurrencia aparezca primero en la lista. Si no hay números repetidos, devuelve -1.
+*/
+
+function findFirstRepeated(gifts) {
+  // Code here
+  let seen = {};
+  for (let id of gifts) {
+    if (seen[id]) {
+      return id;
+    }
+    seen[id] = true;
+  }
+  return -1;
+}
